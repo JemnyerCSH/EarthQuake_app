ebf219a2031cea81bdb0e47a329c5ae6dc7588cf
JemnyerCSH <siewhsuan@nlp.csie.ntust.edu.tw>
2024-11-10 13:01:48 +0800

    描述此次更改的訊息

--- app.py
+++ app.py
@@ -130,4 +130,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
