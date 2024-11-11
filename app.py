4097f575c3cb9b43c181a90ed1988993ca6fd522
JemnyerCSH <siewhsuan@nlp.csie.ntust.edu.tw>
2024-11-12 04:12:04 +0800

    update

--- app.py
+++ app.py
@@ -130,14 +130,12 @@ def ask_taide():
     
     return Response(generate_stream(), mimetype="text/event-stream")
 
-# @app.route("/")
-# def home():
-#     return render_template("index.html")
-
 # 主頁（包含前端界面）
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    # app.run(debug=True, host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
