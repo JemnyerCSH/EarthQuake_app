340b104fdb039d773097f26d22ba547f4a61b546
JemnyerCSH <siewhsuan@nlp.csie.ntust.edu.tw>
2024-11-12 04:20:54 +0800

    update

--- app.py
+++ app.py
@@ -138,4 +138,4 @@ def index():
 if __name__ == '__main__':
     # app.run(debug=True, host="0.0.0.0", port=5001)
     port = int(os.environ.get("PORT", 5001))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
