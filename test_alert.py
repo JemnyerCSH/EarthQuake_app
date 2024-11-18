4f1e135d0e3857e4f1b843f8f0e94d53e72ec71a
JemnyerCSH <siewhsuan@nlp.csie.ntust.edu.tw>
2024-11-18 11:49:14 +0800

    “?”

--- test_alert.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def send_alert():
-    response = requests.get('http://127.0.0.1:5001/earthquake_notifications')
-    print(response.json())
-
-if __name__ == "__main__":
-    send_alert()
