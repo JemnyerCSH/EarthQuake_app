f08684c1361578063eab83c3827d0ef44a063e53
JemnyerCSH <siewhsuan@nlp.csie.ntust.edu.tw>
2024-11-21 21:58:22 +0800

    update

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def load_model():
         device_map="auto", # balanced or auto
         offload_folder="./offload",
         offload_state_dict=True,  # 啟用狀態字典卸載
-        # trust_remote_code=True,
+        trust_remote_code=True,
         torch_dtype="auto",  # 自動選擇數據類型
         low_cpu_mem_usage=True
     )
